d61e08813c2bd28291c3fe3cae4009ae082ea1f3
Renato Priolo <rpriolo@gmail.com>
2022-08-04 12:19:56 -0300

    Update border-radius-previewer.js

---
+++ js/border-radius-previewer.js
@@ -0,0 +1,11 @@
+let box = document.querySelector('#box');
+
+let topLeft = document.querySelector('#top-left');
+let topRight = document.querySelector('#top-right');
+let bottomLeft = document.querySelector('#bottom-left');
+let bottomRight = document.querySelector('#bottom-right');
+
+box.style.borderTopLeftRadius = '25px';
+box.style.borderTopRightRadius = '25px';
+box.style.borderBottomLeftRadius = '25px';
+box.style.borderBottomRightRadius = '25px';
