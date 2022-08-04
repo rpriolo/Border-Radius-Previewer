8991ecef4d9e1e11b7d8f748654a133b1a15aafb
Renato Priolo <rpriolo@gmail.com>
2022-08-04 15:55:50 -0300

    Functions 'setBorderRadius', 'setDefaultValue' and 'generateCode' created

--- js/border-radius-previewer.js
+++ js/border-radius-previewer.js
@@ -1,11 +1,58 @@
-let box = document.querySelector('#box');
+let box = document.querySelector('.box');
 
 let topLeft = document.querySelector('#top-left');
 let topRight = document.querySelector('#top-right');
 let bottomLeft = document.querySelector('#bottom-left');
 let bottomRight = document.querySelector('#bottom-right');
 
-box.style.borderTopLeftRadius = '25px';
-box.style.borderTopRightRadius = '25px';
-box.style.borderBottomLeftRadius = '25px';
-box.style.borderBottomRightRadius = '25px';
+function setBorderRadius() {
+    box.style.borderTopLeftRadius = `${topLeft.value}px`;
+    box.style.borderTopRightRadius = `${topRight.value}px`;
+    box.style.borderBottomRightRadius = `${bottomRight.value}px`;
+    box.style.borderBottomLeftRadius = `${bottomLeft.value}px`;
+}
+
+topLeft.addEventListener('input', setBorderRadius);
+topRight.addEventListener('input', setBorderRadius);
+bottomLeft.addEventListener('input', setBorderRadius);
+bottomRight.addEventListener('input', setBorderRadius);
+
+
+function generateCode() {
+
+    setDefaultValue();
+
+    let option1 = `.box {
+        border-top-left-radius: ${topLeft.value}px;
+        border-top-right-radius: ${topRight.value}px;
+        border-bottom-right-radius: ${bottomRight.value}px;
+        border-bottom-left-radius: ${bottomLeft.value}px;
+    }`
+
+    let option2 = `.box {
+        border-radius: ${topLeft.value}px ${topRight.value}px ${bottomRight.value}px ${bottomLeft.value}px;
+    }`
+
+    console.log(option1)
+    console.log(option2)
+    
+}
+
+
+function setDefaultValue() {
+    if (topLeft.value == '') {
+        topLeft.value = 0;
+    }
+
+    if (topRight.value == '') {
+        topRight.value = 0;
+    }
+
+    if (bottomLeft.value == '') {
+        bottomLeft.value = 0;
+    }
+
+    if (bottomRight.value == '') {
+        bottomRight.value = 0;
+    }
+}
